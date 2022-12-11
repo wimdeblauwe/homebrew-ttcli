4fe6750d65d3dca9681f8c74e79c448c2f5d33dc
jreleaserbot <jreleaser@kordamp.org>
2022-12-11 13:16:08 +0000

    Bump ttcli to 0.5.0.

--- Formula/ttcli.rb
+++ Formula/ttcli.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.3.1 at 2022-11-28T06:35:55.509693369Z
+# Generated with JReleaser 1.3.1 at 2022-12-11T13:15:55.038647353Z
 class Ttcli < Formula
   desc "CLI interface to make working with Thymeleaf projects easier"
   homepage "https://spring.io/projects/spring-boot/ttcli"
-  url "https://github.com/wimdeblauwe/ttcli/releases/download/0.4.0/ttcli-0.4.0-osx-x86_64.zip"
-  version "0.4.0"
-  sha256 "5c7189fdd17284a4b8d3b12b4adf95d96ba113b14c127d88c81ec77202375cf5"
+  url "https://github.com/wimdeblauwe/ttcli/releases/download/0.5.0/ttcli-0.5.0-osx-x86_64.zip"
+  version "0.5.0"
+  sha256 "1949d230e9b197a1d77a5323bb91c6e433c9c5e8587d6633a216df003019e9b8"
   license "Apache License, Version 2.0"
 
 
@@ -15,6 +15,6 @@ class Ttcli < Formula
 
   test do
     output = shell_output("#{bin}/ttcli --version")
-    assert_match "0.4.0", output
+    assert_match "0.5.0", output
   end
 end
