330a6fcd95287c6fbb54ebf37ada4764ccdb195b
jreleaserbot <jreleaser@kordamp.org>
2023-05-09 11:06:08 +0000

    Bump ttcli to 0.9.2.

--- Formula/ttcli.rb
+++ Formula/ttcli.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.5.1 at 2023-05-09T06:31:26.879117574Z
+# Generated with JReleaser 1.5.1 at 2023-05-09T11:05:52.311686001Z
 class Ttcli < Formula
   desc "CLI interface to make working with Thymeleaf projects easier"
   homepage "https://spring.io/projects/spring-boot/ttcli"
   url "https://github.com/wimdeblauwe/ttcli/releases/download/0.9.2/ttcli-0.9.2-osx-x86_64.zip"
   version "0.9.2"
-  sha256 "a1a091d9e19ca17cb5f4581f7b6c98be69a49c1a5c01409ee43c15a60338bcc9"
+  sha256 "619f978db3f28c07de058d52f6be053e37734bc3eaf7bbcef2dc5380a7b402b0"
   license "Apache License, Version 2.0"
 
 
