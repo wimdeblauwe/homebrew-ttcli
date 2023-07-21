0ed7b2378685212694514cdaf62177fdd741d29b
jreleaserbot <jreleaser@kordamp.org>
2023-07-21 14:03:33 +0000

    Bump ttcli to 0.12.0.

--- Formula/ttcli.rb
+++ Formula/ttcli.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.7.0 at 2023-07-19T18:27:45.798570364Z
+# Generated with JReleaser 1.7.0 at 2023-07-21T14:03:18.555239627Z
 class Ttcli < Formula
   desc "CLI interface to make working with Thymeleaf projects easier"
   homepage "https://github.com/wimdeblauwe/ttcli"
-  url "https://github.com/wimdeblauwe/ttcli/releases/download/0.11.0/ttcli-0.11.0-osx-x86_64.zip"
-  version "0.11.0"
-  sha256 "7b50aee16c486d64214b7dc7b84383f1b7ce5698315458a14107bee74dac5e1f"
+  url "https://github.com/wimdeblauwe/ttcli/releases/download/0.12.0/ttcli-0.12.0-osx-x86_64.zip"
+  version "0.12.0"
+  sha256 "c2256aa167f805519ff9b2c9b7d55cac30f7c6253dea7a767894bc948f7ebf94"
   license "Apache License, Version 2.0"
 
 
@@ -15,6 +15,6 @@ class Ttcli < Formula
 
   test do
     output = shell_output("#{bin}/ttcli --version")
-    assert_match "0.11.0", output
+    assert_match "0.12.0", output
   end
 end
